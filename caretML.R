27a9872e4f53aa0c081a8947cd270e28bd02881d
Anant Mittal <mittal.anant@gmail.com>
2017-04-28 16:27:33 +0530

    Basic ML

--- caretML.R
+++ caretML.R
@@ -1,2 +1,28 @@
 library(ggplot2)
 ggplot(data = mtcars, aes(x = wt, y = mpg)) + geom_point()
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Build model using train()
+#~~~~~~~~~~~~~~~~~~~~~~~~~~
+library(caret)
+model.mtcars_lm <- train(mpg ~ wt
+                         ,data = mtcars
+                         ,method = "lm"
+)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Retrieve coefficients for
+# - slope
+# - intercept
+#~~~~~~~~~~~~~~~~~~~~~~~~~~
+coef.icept <- coef(model.mtcars_lm$finalModel)[1]
+coef.slope <- coef(model.mtcars_lm$finalModel)[2]
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Plot scatterplot and regression line
+# using ggplot()
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ggplot(data = mtcars, aes(x = wt, y = mpg)) +
+  geom_point() +
+  geom_abline(slope = coef.slope, intercept = coef.icept, color = "red")
+
+
